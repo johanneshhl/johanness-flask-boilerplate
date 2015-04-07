8179b49152eb59b3e6978db3ebe8f5d1c1dd4828
Johannes Hvilsom Larsen <hvilsom@outlook.com>
2015-04-07 19:57:04 +0200

    Getting started

--- application/__init__.py
+++ application/__init__.py
@@ -2,12 +2,12 @@
  # -*- coding: utf-8 -*-
 
 from flask import Flask
-
-#from flask.ext.heroku import Heroku 
+from flask.ext.heroku import Heroku 
 
 
 app = Flask(__name__)
 app.config.from_object('applicationConfig')
+heroku = Heroku(app)
 
 
 from application.views import views
--- applicationConfig.py
+++ applicationConfig.py
@@ -6,7 +6,7 @@ DEBUG = False
 
 SECRET_KEY = '\xb2\xcd]9\xa6\xad\xaf\xc6\xb1NSz\xdbT\x044\x19\xfac\xf7y\xef\x18\x97'
 
-DATABASE_URI = os.environ.get('DATABASE_URL')
+#DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 #DATABASE_CONNECT_OPTIONS = {}

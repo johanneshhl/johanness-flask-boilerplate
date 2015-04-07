fb5368d2c04358b74788f4c45bc5ae7b188a2f44
Johannes Hvilsom Larsen <hvilsom@outlook.com>
2015-04-07 20:44:02 +0200

    123

--- run.py
+++ run.py
@@ -3,4 +3,9 @@
 
 
 from application import app
-app.run(debug=False)
+
+
+if __name__ == '__main__':
+        import os  
+        port = int(os.environ.get('PORT', 33507)) 
+        app.run(host='0.0.0.0', port=port)

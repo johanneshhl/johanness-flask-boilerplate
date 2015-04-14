00499b3972175653045a36519358313879692e5d
Johannes Hvilsom Larsen <hvilsom@outlook.com>
2015-04-14 14:30:11 +0200

    Secure = true 😄

--- applicationConfig.py
+++ applicationConfig.py
@@ -3,6 +3,8 @@ import os
 if not os.environ.has_key('DATABASE_URL'):
 	DEBUG = True
 else:
+	SESSION_COOKIE_SECURE = True
+	PREFERRED_URL_SCHEME = https
 	DEBUG = False
 
 SECRET_KEY = '\xb2\xcd]9\xa6\xad\xaf\xc6\xb1NSz\xdbT\x044\x19\xfac\xf7y\xef\x18\x97'

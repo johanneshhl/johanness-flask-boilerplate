c056f871d9404d139673b5ee20aaf259612d75ca
Johannes Hvilsom Larsen <hvilsom@outlook.com>
2015-04-17 13:31:19 +0200

    blabla

--- application/views/decorators/decorators.py
+++ application/views/decorators/decorators.py
@@ -2,16 +2,18 @@
  # -*- coding: utf-8 -*-
 
 from functools import wraps
-from application import app, request, redirect, escape, session, url_for, db, bcrypt, render_template, g, flash
+from application import Flask, app, request, redirect, escape, session, url_for, db, bcrypt, render_template, g, flash
 from application.functions.functions import *
 
 
 
 def login_required(f):
     @wraps(f)
+    
     def decorated_function(*args, **kwargs):
+    	app.logger.debug(replaceHTTP(request.url))
         if g.user == None:
-            return redirect(url_for('login', next=replaceHTTP(request.url), _external=True, _scheme="https"))
+            return redirect(url_for('login', next=replaceHTTP(request.url), external=True, scheme="https"))
         return f(*args, **kwargs)
     return decorated_function
 
